ad41f4d3c807d5015dd967577be567a87595b085
YuseiShiozawa <yuyu9000img@gmail.com>
2024-05-13 16:42:59 +0900

    add blue point pro

--- src/line_trace.py
+++ src/line_trace.py
@@ -10,8 +10,15 @@ def main():
     pub = rospy.Publisher('distance', Int32, queue_size=10)
     rate = rospy.Rate(10)  # ループ周波数（10Hz）
 
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(4)
     cap.set(cv2.CAP_PROP_FPS, 30)
+    cv2.namedWindow('video image', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('video image', 85, 60)
+
+    blue_point_x1 = 100 # 左の青点
+    blue_point_y1 = 60
+    blue_point_x2 = 550 # 右の青点
+    blue_point_y2 = 60 
 
     while not rospy.is_shutdown():
         ret, img = cap.read()
@@ -19,7 +26,8 @@ def main():
         height, width, _ = img.shape
         img = img[height//2:, :]
         red_point_x = width // 2
-
+       # cv2.imshow('video image', img)
+   
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
@@ -33,9 +41,19 @@ def main():
                     cx = int(M["m10"] / M["m00"])
 
                 distance = (cx - red_point_x) * -1
+
+                if blue_point_y1 >= 0 and blue_point_y1 < img.shape[0] and blue_point_x1 >= 0 and blue_point_x1 < img.shape[1]:
+                    if binary[blue_point_y1, blue_point_x1] != 0:
+#                        distance = 77777
+                        if blue_point_y2 >= 0 and blue_point_y2 < img.shape[0] and blue_point_x2 >= 0 and blue_point_x2 < img.shape[1]:
+                            if binary[blue_point_y2, blue_point_x2] != 0:
+                                distance = 77777
+ 
                 pub.publish(distance)
                 rate.sleep()
 
+        cv2.imshow('video image', img)
+        cv2.waitKey(1)
     cap.release()
 
 if __name__ == '__main__':
